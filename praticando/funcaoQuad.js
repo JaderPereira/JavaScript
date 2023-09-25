6cdd716c81ec9e6bb39aa9875babc20176dc1867
Jader Gomes <141690837+JaderPereira@users.noreply.github.com>
2023-09-25 00:25:34 -0300

    programa função quadrada att

--- praticando/funcaoQuad.js
+++ praticando/funcaoQuad.js
@@ -3,35 +3,51 @@
 //Xv = -b : (2*a)
 //Yv = -delta : (4*a)
 
-let abc = [1, 4, 3]         //A, B, e C
+let abc = [1, -4, 3]         //A, B, e C
+
 let concavidade
+
+let x = 1
+
+let delta = (abc[1] * abc[1]) - 4 * (abc[0] * abc[2])
+
+if (abc[0] > 0) {
+    concavidade = "concavidade pra cima"
+} else if (abc[0] < 0) {
+    concavidade = "concavidade para baixo"
+}
+
+function raiz (delta) {
+    let raizDelta
+    
+    for(let aux = 0 ; aux < delta; aux++) {
+        if ((aux*aux) == delta) {
+            raizDelta = aux
+        }
+    }
+
+    let x1 = (((-1) * abc[1]) + raizDelta) / (2 * abc[0])
+    let x2 = (((-1) * abc[1]) - raizDelta) / (2 * abc[0])
+
+    return console.log("X1 da função é:",x1,". E x2 é:",x2)
+}
 
 function vertice () {
     let verticeX = ((-1) * abc[1]) / (2 * abc[0])
     
-    let verticeY = (-1) * delta() / (4 * abc[0])
+    let verticeY = (-1) * delta / (4 * abc[0])
     
     return console.log("o valor do X do vértice é:",verticeX,", e o valor do vértice Y é:",verticeY,". V=(",verticeX,",",verticeY,")")
 }
 
-function delta () {
-    let form = (abc[1] * abc[1]) - 4 * (abc[0] * abc[2])
-    return form
-}
-
 function f_numero (x) {
     let form = (abc[0] * (x*x)) + (abc[1] * x) + abc[2]
-
-    if (abc[0] > 0) {
-        concavidade = "concavidade pra cima"
-    } else if (abc[0] < 0) {
-        concavidade = "concavidade para baixo"
-    }
-
+    
     return form
 }
 
-console.log("função:",f_numero(3))
+console.log("função:",f_numero(x))
 console.log(concavidade)
+raiz(delta)
 vertice()
 console.log("Ponto C:",abc[2])
