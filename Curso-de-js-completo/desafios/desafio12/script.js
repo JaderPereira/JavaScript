a5cf032386eb5b7be9909a4e6773546946cc4ccd
Jader Gomes <141690837+JaderPereira@users.noreply.github.com>
2024-05-03 18:15:05 -0300

    css e html do exercicio de criar uma calculadora com factory functions
    Um exercício para criar uma calculadora com uma factory function vai começar e eu já completei a parte do CSS e HTML, agora só falta a do JS :)
    Teve umas alterações no arquivo de factory functions também mas foi pouca coisa

---
+++ Curso-de-js-completo/desafios/desafio12/script.js
@@ -0,0 +1,9 @@
+//Código da calculadora feita com uma factory function.
+/*Súmario
+    .btn = todos os botões
+    .btn-eq = Botão de igual
+    .btn-clear = botão de apagar tudo
+    .btn-del = botão de apagar 1 bagulho
+    .btn-num = Todos os botões de números
+    .display = telinha aonde aparece a conta e o resultado
+*/
--- Curso-de-js-completo/funcoes/avacado/factoryFunction.js
+++ Curso-de-js-completo/funcoes/avacado/factoryFunction.js
@@ -6,16 +6,43 @@ function criaPessoa (nome, sobrenome, altura, peso) {
         peso,
 
         //da pra declarar um metodo assim e assim: "fala(assunto) {...}"
-        fala: function (assunto) {
+        fala: function (assunto = 'falando sobre nada') {
             return `${nome} está ${assunto}.`
         },
+
+        //getter
         get imc() {
             const indice = this.peso / (this.altura ** 2)
             return indice.toFixed(2)
+        },
+
+        get nomeCompleto() {
+            return `${this.nome} ${this.sobrenome}`
+        },
+
+        //setter
+        set nomeCompleto (valor) {
+            /*o método split vai fazer com que uma string seja dividada entre seus espaços e crie um array, cada indice contem o valor da string entre os espaços, exemplo: 
+                nome = "Jader Pereira Santos"
+                nome.split(' ')
+                console.log(nome)   O resultado vai ser um array com o primeiro item do índice sendo "Jader", o segundo sendo "Pereira" e o terceiro "Santos"
+
+            obs: bota um espaço no parâmetro desse método pq se n ele vai pegar cada letra como se fosse um elemento, testa ai pra tu ver sem esse espacinho        */
+            valor = valor.split(' ')
+            console.log(valor)
+            //shift vai remover o primeiro índice do array
+            this.nome = valor.shift()
+            //o método join vai fazer com que o array se torne uma string, seu parâmetro é o que vai ficar entre os valores do array
+            this.sobrenome = valor.join(' ')
+            console.log(valor)
         }
     }
 }
 const p1 = criaPessoa('Jader', 'Pereira', 1.7, 55)
+p1.nomeCompleto = 'Thales Souza Oliveira'
 console.log(p1.fala('falando sobre JS'))
+console.log(p1.nome)
 console.log(p1.sobrenome)
 console.log(p1.imc)
+console.log(p1.nomeCompleto)
+
