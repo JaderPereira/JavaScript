6f3662afdb27616742080df32c11f51a1a22829f
Jader Gomes <141690837+JaderPereira@users.noreply.github.com>
2025-01-11 00:22:52 -0300

    aula sobre middlewares
    Aprendi sobre middleware e suas funções, também descobri q já estava utilizando eles mas n sabia seu nome.

---
+++ node/express/middlewares/src/controllers/homeController.js
@@ -0,0 +1,13 @@
+exports.paginaInicial = (req, res, next) => {
+    //essa parada de session se refere a sessão do usuário, mt util para pegar informações ali na hora dele como um nome de um usuário q ele colocou e colocar em outras partes do programa, o objeto session ajuda a gente nisso, eu coloquei o nome de maneira bruta la no routes mas peguei ele por aqui usando o req.session.
+    console.log('Olha o que tem dentro do req.session.nome:', req.session.nome)
+    console.log('eu to entre o beforeMiddleware e antes do afterMiddleware')
+    res.render('index')
+    next() //da pra usar o next aqui tbm
+}
+
+exports.trataPOST = (req, res) => {
+    res.send(req.body)
+}
+
+
